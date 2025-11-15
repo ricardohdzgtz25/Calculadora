ede2db29b6f97f2ad6ac40bf34df714bded96aee
RicardoHdzGtz <hernandezrgtz@gmail.com>
2025-11-14 18:12:21 -0600

    implementar función click_boton para manejar la entrada de valores en la calculadora
    Se agregó la función click_boton(valor) encargada de obtener el contenido actual del display, actualizarlo y unir el valor presionado por el usuario.
    Esta función permite construir operaciones matemáticas en la pantalla de la calculadora.

--- src/calculadora.py
+++ src/calculadora.py
@@ -2,3 +2,25 @@ import tkinter as tk
 from tkinter import ttk
 
 # FUNCIONES DE LA CALCULADORA
+def click_boton(valor):
+    entrada_actual = pantalla.get()
+    pantalla.delete(0, tk.END)
+    pantalla.insert(0, entrada_actual + str(valor))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
