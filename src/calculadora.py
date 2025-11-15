d0428b383ff4be918dd9bffff6fcee553e5042e4
Jenpa02 <jennypa200384@gmail.com>
2025-11-14 19:48:40 -0600

    lista de botones

--- src/calculadora.py
+++ src/calculadora.py
@@ -35,6 +35,18 @@ pantalla = tk.Entry(ventana, font=("Arial", 24), justify="right")
 pantalla.pack(fill="both", padx=10, pady=10)
 
 
+# BOTONES
+botones = [
+    ("7", 1, 0), ("8", 1, 1), ("9", 1, 2), ("/", 1, 3),
+    ("4", 2, 0), ("5", 2, 1), ("6", 2, 2), ("*", 2, 3),
+    ("1", 3, 0), ("2", 3, 1), ("3", 3, 2), ("-", 3, 3),
+    ("0", 4, 0), (".", 4, 1), ("=", 4, 2), ("+", 4, 3)
+]
+
+frame_botones = tk.Frame(ventana)
+frame_botones.pack()
+
+
 
 
 
