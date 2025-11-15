82d916b5212766cb9db491170609957a27c9f058
RicardoHdzGtz <hernandezrgtz@gmail.com>
2025-11-14 18:25:29 -0600

    Agregar funciones de borrado parcial y total en la calculadora
    Se implementaron las funciones borrar_uno() y borrar_todo(), la primera elimina el último carácter del display para permitir correcciones rápidas, mientras que la segunda limpia completamente la pantalla de la calculadora.

--- src/calculadora.py
+++ src/calculadora.py
@@ -7,6 +7,14 @@ def click_boton(valor):
     pantalla.delete(0, tk.END)
     pantalla.insert(0, entrada_actual + str(valor))
 
+def borrar_uno():
+    entrada_actual = pantalla.get()
+    pantalla.delete(0, tk.END)
+    pantalla.insert(0, entrada_actual[:-1])
+
+def borrar_todo():
+    pantalla.delete(0, tk.END)
+
 
 
 
