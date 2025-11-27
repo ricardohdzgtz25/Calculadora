f9e521031208c47b426751a8ffde4ce4a7811bdd
RicardoHdzGtz <hernandezrgtz@gmail.com>
2025-11-27 15:37:07 -0600

    Creación del diccionario con funciones y constantes seguras
    Se define un diccionario que contiene únicamente las funciones matemáticas
    que podrán ser usadas en la evaluación segura de expresiones

--- calculadora_cientifica.py
+++ calculadora_cientifica.py
@@ -9,3 +9,12 @@ def asin_deg(x): return math.degrees(math.asin(x))
 def acos_deg(x): return math.degrees(math.acos(x))
 def atan_deg(x): return math.degrees(math.atan(x))
 
+ALLOWED_NAMES = {
+    'pi': math.pi, 'e': math.e,
+    'sin': sin_deg, 'cos': cos_deg, 'tan': tan_deg,
+    'asin': asin_deg, 'acos': acos_deg, 'atan': atan_deg,
+    'ln': math.log, 'log': math.log, 'exp': math.exp,
+    'sqrt': math.sqrt, 'pow': math.pow, 'abs': abs
+}
+
+OPERADORES = "+-*/^."
