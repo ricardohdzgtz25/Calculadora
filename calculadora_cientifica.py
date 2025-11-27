c86eb6928e4df01cbf624d7de715fdf7b3a10263
RicardoHdzGtz <hernandezrgtz@gmail.com>
2025-11-26 22:44:24 -0600

    Diccionario de funciones permitidas
    Define funciones y constantes matemáticas las cuales estarán permitidas para que podamos evaluar expresiones de forma segura.

--- calculadora_cientifica.py
+++ calculadora_cientifica.py
@@ -1,3 +1,19 @@
 import tkinter as tk
 import math
 
+ALLOWED_NAMES = {
+    'pi': math.pi,
+    'e': math.e,
+    'sin': math.sin,
+    'cos': math.cos,
+    'tan': math.tan,
+    'asin': math.asin,
+    'acos': math.acos,
+    'atan': math.atan,
+    'ln': math.log,
+    'log': math.log,
+    'exp': math.exp,
+    'sqrt': math.sqrt,
+    'pow': math.pow,
+    'abs': abs
+}
